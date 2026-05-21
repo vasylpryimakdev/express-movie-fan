e9eea6066e2e0d9ff035172e9fffa7ed245995f9
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-21 15:21:34 +0300

    refactor: improve API routes, add search and update UI styles

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,64 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
+const request = require('request');
 
-const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
-const apiBaseUrl = "http://api.themoviedb.org/3";
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
 const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
-const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
-router.use((req, res, next) => {
+router.use((req, res, next)=>{
   res.locals.imageBaseUrl = imageBaseUrl;
 
-  next();
-});
+  next()
+})
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  request.get(nowPlayingUrl, (error, response, movieData) => {
+router.get('/', function(req, res, next) {
 
-    console.log(movieData);
+  request.get(nowPlayingUrl,(error, response, movieData)=>{
 
-    const parsedData = JSON.parse(movieData);
+    console.log(movieData)
 
-    res.render("index", {
+    const parsedData = JSON.parse(movieData)
+
+    res.render('index',{
       parsedData: parsedData.results,
-    });
-  });
+    })
+  })
 });
 
-router.get("/movie/:id", (req, res, next) => {
+router.get('/movie/:id',(req, res, next)=>{
   const movieId = req.params.id;
-  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`
 
-  request.get(thisMovieUrl, (error, response, movieData) => {
-    console.log(typeof movieData);
+  request.get(thisMovieUrl,(error, response, movieData)=>{
+    console.log(typeof(movieData))
 
-    const parsedData = JSON.parse(movieData);
+    const parsedData = JSON.parse(movieData)
 
-    res.render("single-movie", {
-      parsedData,
-    });
-  });
-});
+    res.render('single-movie',{
+      parsedData
+    })
+  })
+})
+
+router.post('/search',(req, res, next)=>{
+  const userSearchTerm = encodeURI(req.body.movieSearch);
+  const cat = req.body.cat;
+  const movieUrl = `${apiBaseUrl}/search/${cat}?query=${userSearchTerm}&api_key=${apiKey}`
+
+  request.get(movieUrl,(error, response, movieData)=>{
+    let parsedData = JSON.parse(movieData);
+
+    if(cat=="person"){
+      parsedData.results = parsedData.results[0].known_for;
+    }
+
+    res.render('index', {
+      parsedData: parsedData.results
+    })
+  })
+})
 
 module.exports = router;
