ec00d3808a5272b48e530dacab5a3852a6798edd
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-21 14:09:49 +0300

    feat: fetch movies from TMDB API and render results on home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const apiKey = "1fb720b97cc13e580c2c35e1138f90f8";
+const apiBaseUrl = "http://api.themoviedb.org/3";
+const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
+const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    console.log(movieData);
+
+    const parsedData = JSON.parse(movieData);
+    
+    res.render("index", {
+      parsedData: parsedData.results,
+    });
+  });
 });
 
 module.exports = router;
